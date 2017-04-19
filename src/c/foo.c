c86bfe367e11c45b58049ce09c68c50fdf341128
gauthier <gauthierbouyjou@aol.com>
2017-04-19 19:26:38 +0200

    test

--- src/c/foo.c
+++ src/c/foo.c
@@ -3,16 +3,16 @@
 #include <stdbool.h>
 
 int main(void) {
-    bool is_value = true;
+	bool is_value = true;
 
-    printf("This line starts at four!\n");
-    if (is_value) {
-	printf("This line starts at nine!\n");
-    }
-    else
-    {
-	printf("Shut the fuck up man\n");
-    }
+	printf("This line starts at four!\n");
+	if (is_value) {
+		printf("This line starts at nine!\n");
+	}
+	else
+	{
+		printf("Shut the fuck up man\n");
+	}
 
-    return 0;
+	return 0;
 }
