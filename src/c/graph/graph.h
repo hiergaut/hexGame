ffc1cc8a4118267f1691b3f0f89c891aac9b62dc
gauthier <gauthierbouyjou@aol.com>
2017-04-20 09:38:19 +0200

    new rep graph

---
+++ src/c/graph/graph.h
@@ -0,0 +1,12 @@
+#ifndef __GRAPH__
+#define __GRAPH__
+
+typedef struct s_graph* graph;
+
+graph graph_create();
+void graph_destroy(graph* g);
+void graph_insertVertex(graph g ,const void* data);
+void graph_insertEdge(graph g ,const void* data ,const void* data2);
+void graph_print(graph g);
+
+#endif
---
+++ src/c/graph/main.c
@@ -0,0 +1,8 @@
+#include "graph.h"
+
+int main() {
+
+
+
+    return 0;
+}
