52879929bbd2f5b3a2727878776e2d2e85b532ef
gauthier <gauthierbouyjou@aol.com>
2017-04-24 22:34:57 +0200

    double interface.c

--- src/c/interfaceAvecJava/Main.java
+++ src/c/interfaceAvecJava/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
@@ -7,27 +7,27 @@ public class Main {
 	InterfaceAvecC.setPawn(InterfaceAvecC.BLACK_PAWN, 2, 2);
 	InterfaceAvecC.setPawn(InterfaceAvecC.WHITE_PAWN, 3, 2);
 
-	Scanner reader = new Scanner(System.in);  // Reading from System.in
-	System.out.println("Enter a number: ");
-	int n = reader.nextInt(); // Scans the next token of the input as an int.
-	// if (InterfaceAvecC.permission(2, 4))
-	//     System.out.println("Oh yeah");
-        //
-	// InterfaceAvecC.setPawn(InterfaceAvecC.WHITE_PAWN, 2, 4);
-        //
-	// if (InterfaceAvecC.hasAWinner() !=0)
-	//     System.out.println("Winner");
-        //
-	// InterfaceAvecC.saveGame();
-	// InterfaceAvecC.restoreGame(5);
-	// InterfaceAvecC.undo();
-        //
-	// InterfaceAvecC.displayHistoryGame();
-	// InterfaceAvecC.getPawn(3, 5);
-        //
-	// InterfaceAvecC.displayGraphGame();
-	// InterfaceAvecC.displayPlateau();
-	// InterfaceAvecC.displayGroup(InterfaceAvecC.BLACK_PAWN);
+	// Scanner reader = new Scanner(System.in);  // Reading from System.in
+	// System.out.println("Enter a number: ");
+	// int n = reader.nextInt(); // Scans the next token of the input as an int.
+	if (InterfaceAvecC.permission(2, 4))
+	    System.out.println("Oh yeah");
+
+	InterfaceAvecC.setPawn(InterfaceAvecC.WHITE_PAWN, 2, 4);
+
+	if (InterfaceAvecC.hasAWinner() !=0)
+	    System.out.println("Winner");
+
+	InterfaceAvecC.saveGame();
+	InterfaceAvecC.restoreGame(5);
+	InterfaceAvecC.undo();
+
+	InterfaceAvecC.displayHistoryGame();
+	InterfaceAvecC.getPawn(3, 5);
+
+	InterfaceAvecC.displayGraphGame();
+	InterfaceAvecC.displayPlateau();
+	InterfaceAvecC.displayGroup(InterfaceAvecC.BLACK_PAWN);
 
 	InterfaceAvecC.endGame();
 
